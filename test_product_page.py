e71e32f1ab08fff5df9a54593201a0e923b27ce2
Pavel Tsirulnikov <pavel.tsirulnikov@Pavels-MacBook-Pro.local>
2021-03-17 21:07:44 +0100

    task 3 parameterization added

--- test_product_page.py
+++ test_product_page.py
@@ -1,3 +1,4 @@
+import pytest
 import time
 from pages.product_page import ProductPage
 
@@ -7,10 +8,26 @@ from pages.product_page import ProductPage
 #     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
 #     page.open()                      # открываем страницу
 #     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+# @pytest.mark.parametrize('link', ["okay_link",
+#                                   pytest.param("bugged_link", marks=pytest.mark.xfail),
+#                                   "okay_link"])
 
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  pytest.param("bugged_link", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+# @pytest.mark.xfail(reason="fixing this bug right now")
+
+def test_guest_can_add_product_to_basket(browser, link):
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_add_to_basket_button()
